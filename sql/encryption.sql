a59f9644342454c7ed5136ae71bc41e8306bc7dd
jpipan <janpipan@gmail.com>
2023-11-10 19:35:52 +0100

    fixed encryption table

--- sql/encryption.sql
+++ sql/encryption.sql
@@ -1,9 +1,10 @@
 CREATE TABLE encryption (
-    id INT PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     picture_id INT,
     key_salt BLOB,
     password_salt BLOB,
     init_vector BLOB,
     password_hash VARCHAR(255),
-    FOREIGN KEY (picture_id) REFERENCES Image(id)
+    FOREIGN KEY (picture_id) REFERENCES Image(id),
+    PRIMARY KEY (Id)
 )
